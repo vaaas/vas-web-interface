ec48e9e47d0167b987205bc98700f7883b18b6fb
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-08-13 16:20:26 +0300

    move all user defined functions elsewhere

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const error_responses = require('error-responses')
 const get_file = require('get-file')
 
 const CONF = JSON.parse(fs.readFileSync(__dirname + '/conf.json').toString('utf8'))
+const LOGIN_FORM = () => get_file(__dirname + '/login_form.xhtml')
+const last = x => x[x.length-1]
+const authorise = f => req => req.cookies.p === CONF.password ? f(req) : LOGIN_FORM()
+const FUNCTIONS = {}
 
 module.exports = async (x) => route(parse_url(parse_cookies(x)))
-
-const LOGIN_FORM = () => get_file(__dirname + '/login_form.xhtml')
-
-const authorise = f => req => req.cookies.p === CONF.password ? f(req) : LOGIN_FORM()
 
 const user_function = authorise(req => read_post_data(req).then(x => ({
 	status: 200,
@@ -24,14 +24,38 @@ const user_function = authorise(req => read_post_data(req).then(x => ({
 	data: JSON.stringify(eval(x.toString()))
 })))
 
+const defined_function = authorise(async (req) => {
+	const fn = FUNCTIONS[req.pathname.slice(1)]
+	if (fn === undefined) return error_responses.not_found
+	const args = JSON.parse((await read_post_data(req)).toString())
+	return {
+		status: 200,
+		data: JSON.stringify(fn(...args)),
+		mimetype: 'application/json',
+		headers: []
+	}
+})
+
+const define_functions = authorise(async (req) => {
+	const data = JSON.parse((await read_post_data(req)).toString())
+	for (const k in data)
+		FUNCTIONS[k] = eval(data[k])
+	return {
+		status: 200,
+		data: 'OK',
+		mimetype: 'text/plain',
+		headers: [],
+	}
+})
+
 async function login(req) {
-	const data = (await read_post_data(req)).toString()
+	const data = await read_post_data(req).then(x => x.toString())
 	if (data === 'p=' + CONF.password)
 		return {
 			status: 303,
 			headers: [
 				[ 'set-cookie', 'p=' + CONF.password ],
-				[ 'Location', req.headers.referer ?? '/' ],
+				[ 'Location', req.headers.referer || '/' ],
 			],
 			data: 'Login successful',
 			mimetype: 'text/plain',
@@ -63,14 +87,6 @@ function static_directory(f) {
 	}
 }
 
-const stylesheet = `
-body
-	{ font-family: monospace;
-	line-height: 1.5em;
-	word-wrap: break-word; }
-li + li { margin-top: 0.5em; }
-`.trim()
-
 function route(req) {
 	switch (req.method) {
 		case 'GET':
@@ -79,12 +95,18 @@ function route(req) {
 			switch(req.pathname) {
 				case '/': return user_function(req)
 				case '/login': return login(req)
-				default: return error_responses.not_found
+				case '/define': return define_functions(req)
+				default: return defined_function(req)
 			}
 		default:
 			return error_responses.method_not_allowed
 	}
 }
+
+const stylesheet = `
+body { font-family: monospace; line-height: 1.5em; word-wrap: break-word; }
+li + li { margin-top: 0.5em; }
+`.trim()
 
 function directories_template(xs) {
 	let tree = xs.map(x => ['li', null, [['a', [['href', x]], [last(x.split('/'))]]]])
@@ -99,15 +121,3 @@ function directories_template(xs) {
 		]]
 	return '<!DOCTYPE html>' + serialise_html(tree)
 }
-
-function* walk_directory(dir) {
-	for (const name of fs.readdirSync(dir)) {
-		if (name[0] === '.') continue
-		const pathname = path.join(dir, name)
-		const stats = fs.statSync(pathname)
-		if (stats.isDirectory()) yield* walk_directory(pathname)
-		else if (stats.isFile()) yield pathname
-	}
-}
-
-const last = x => x[x.length-1]
